e939ad76da9ccaaa9cac5701d90cb692eb0ccea7
SwiftShimizu <shimizuswiftforlearning@gmail.com>
2025-02-02 19:12:52 +0900

    Initial Commit

---
+++ Memory_Concentration/Memory_ConcentrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memory_ConcentrationApp.swift
+//  Memory_Concentration
+//
+//  Created by 志水拓哉 on 2025/02/02.
+//
+
+import SwiftUI
+
+@main
+struct Memory_ConcentrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
